3810aec36e2869ace1fd6cd6bdba2001151e6ab0
ELOI BRITO DE JESUS <e.jesus@spotpromo.com.br>
2019-07-14 16:20:29 -0300

    projeto accenture

---
+++ app/src/main/java/br/com/projetoaccenturebank/util/Utils.kt
@@ -0,0 +1,48 @@
+package br.com.projetoaccenturebank.util
+
+import java.util.HashMap
+import java.util.regex.Pattern
+
+object Utils {
+
+    fun validaSenha(dados: String, tipo: Int): Boolean {
+
+        if (tipo == 1)
+            if (dados.isEmpty())
+                return false
+
+
+        if (tipo == 2) {
+            if(dados.isEmpty())
+                return false
+
+            val expSenha = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%]).{6,20})"
+
+            return expSenha.toRegex().matches(dados)
+        }
+
+        return true
+    }
+
+    /**
+     * RETORNA HASH MAP
+     *
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    @Throws(Exception::class)
+    fun retornaHashJson(type: String, vararg params: Any): HashMap<String, String> {
+        val mHash = HashMap<String, String>()
+
+        if (params.size > 0) {
+            for (`object` in params) {
+                val key = (`object` as String).split(type.toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                if (key.size >= 2)
+                    mHash[key[0]] = key[1]
+
+            }
+        }
+        return mHash
+    }
+}
